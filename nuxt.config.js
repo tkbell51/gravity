0bdd811aeabf76cf2014999187d6ace928783612
Tim Bell <tkbell51@gmail.com>
2020-05-14 19:58:57 -0400

    uninstalled pwa

--- nuxt.config.js
+++ nuxt.config.js
@@ -59,7 +59,6 @@ export default {
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
     '@nuxtjs/axios',
-    '@nuxtjs/pwa',
     // Doc: https://github.com/nuxt-community/dotenv-module
     '@nuxtjs/dotenv',
     '@nuxtjs/style-resources',
