a9ca211b3bf73de386603e6efed792824349506e
Tim Bell <tkbell51@gmail.com>
2024-12-20 10:15:56 -0500

    all content need styling

--- pages/cost-services.vue
+++ pages/cost-services.vue
@@ -73,9 +73,81 @@
             <div>
               <h3 class="heading-tertiary">GROUP Self Pay Sessions</h3>
               <p>Men's Wellness Group (90 minutes) - $FREE</p>
+              <p>Managing Anger Appropriately II (60 minutes) - $25</p>
+              <p>Managing Anger Appropriately II (60 minutes) - $25</p>
+              <p>Positively Parenting (60 minutes) - $25</p>
             </div>
           </div>
         </div>
+        <h2 class="heading-secondary">Cancellation Policy</h2>
+        <p>
+          For your convenience, Gravity Counseling Group provides optional text
+          reminders to help you stay informed about your upcoming appointments.
+          Please note, if you miss a scheduled therapy session without notifying
+          our office at least 24 hours in advance to cancel or reschedule, a $50
+          fee will be charged. This fee is not covered by insurance and applies
+          to late cancellations or no-show appointments. Thank you for your
+          understanding and cooperation.
+        </p>
+
+        <h2 class="heading-secondary">Good Faith Estimate</h2>
+        <p>
+          Effective January 1, 2022, the "No Surprises Act" (Section 2799B-6 of
+          the Public Health Service Act) requires healthcare providers to offer
+          a Good Faith Estimate for the cost of out-of-network care.
+        </p>
+        <ul>
+          <li>
+            <p>
+              You are entitled to a Good Faith Estimate explaining the total
+              expected cost of non-emergency services or items.
+            </p>
+          </li>
+          <li>
+            <p>
+              Providers must supply this estimate if requested before scheduling
+              services.
+            </p>
+          </li>
+          <li>
+            <p>
+              It’s essential to save a copy or picture of your Good Faith
+              Estimate for your records.
+            </p>
+          </li>
+        </ul>
+        <p>
+          For additional information about your rights under the No Surprises
+          Act:
+        </p>
+        <ul>
+          <li>
+            <p>
+              Vist
+              <a
+                href="http://www.cms.gov/nosurprises"
+                title="No Surprises Act"
+                target="_blank"
+                >www.cms.gov/nosurprises</a
+              >
+            </p>
+          </li>
+          <li>
+            <p>
+              Contact South Carolina Department of Insurance at
+              <a
+                href="tel:803-737-6160"
+                title="South Carolina Department of Insurance"
+                >803-737-6160</a
+              >
+            </p>
+          </li>
+        </ul>
+        <p>
+          By collaborating with you, our mental health professionals ensure that
+          treatment plans align with your goals, preferences, and financial
+          needs, creating a supportive and informed therapeutic experience.
+        </p>
       </div>
     </section>
   </div>
@@ -137,5 +209,11 @@ export default {
       margin-bottom: 1rem;
     }
   }
+}
+.self-pay-flex {
+  display: flex;
+  margin: 2rem 0;
+  gap: 4rem;
+  justify-content: space-between;
 }
 </style>
